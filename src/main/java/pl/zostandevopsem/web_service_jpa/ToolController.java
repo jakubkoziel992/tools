f4974f04578eb83631f39454a58eb5199f6c75f9
Koziel <jakub.koziel@accenture.com>
2024-06-11 19:41:39 +0200

    remove user - mathos has been added

--- src/main/java/pl/zostandevopsem/web_service_jpa/ToolController.java
+++ src/main/java/pl/zostandevopsem/web_service_jpa/ToolController.java
@@ -57,4 +57,31 @@ public class ToolController {
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+
+    @PatchMapping("tools/{id}")
+    public ResponseEntity<Tool> partiallyUpdateTool(@PathVariable Integer id, @RequestBody Tool tool){
+        return toolRepository.findById(id)
+                .map(existingTool -> {
+                    if (tool.getToolName() != null) existingTool.setToolName(tool.getToolName());
+                    if (tool.getDescription() != null) existingTool.setDescription(tool.getDescription());
+                    if (tool.getStartDate() != null) existingTool.setStartDate(tool.getStartDate());
+
+                    return toolRepository.save(existingTool);
+                })
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("tools/{id}")
+    public ResponseEntity<Void> deleteTool(@PathVariable Integer id) {
+        if (!toolRepository.existsById(id)){
+            return ResponseEntity.notFound().build();
+        }
+
+        toolRepository.deleteById(id);
+
+        return ResponseEntity.noContent().build();
+    }
+
+
 }
