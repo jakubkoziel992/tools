c744490a356265db5346d306ce7605e69c063da7
Koziel <jakub.koziel@accenture.com>
2024-07-04 20:02:07 +0200

    connected application with docker mysql dbv

--- src/main/java/pl/zostandevopsem/web_service_jpa/Tool.java
+++ src/main/java/pl/zostandevopsem/web_service_jpa/Tool.java
@@ -1,10 +1,8 @@
 package pl.zostandevopsem.web_service_jpa;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
+@Table(name = "tools")
 @Entity
 public class Tool {
     @Id
